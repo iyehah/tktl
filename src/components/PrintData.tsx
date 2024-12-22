84d1b7ae1e2f9b07c6829e37e5d499e94dcacb14
Iyehah <117352746+iyehah@users.noreply.github.com>
2024-12-22 14:39:13 +0000

    fix: print data

--- src/components/PrintData.tsx
+++ src/components/PrintData.tsx
@@ -56,7 +56,7 @@ const PrintData: React.FC<PrintDataProps> = ({ tableId }) => {
   return (
     <button
       onClick={handlePrint}
-      className="bg-blue-500 text-white px-4 py-2 rounded"
+      className="bg-green-500 text-white px-4 py-2 rounded :hover:bg-green-600 mt-1"
     >
       طباعة
     </button>
